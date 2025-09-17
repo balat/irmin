cee826b67ad90af4a58a8ae492cd4303a310d5e5
Vincent Balat <vincent.balat@vblt.org>
2025-09-17 16:47:08 +0200

    Version 2 of Fake Lavyek store (in memory) based on irmin-mem

--- src/irmin-lavyek/irmin_lavyek.ml
+++ src/irmin-lavyek/irmin_lavyek.ml
@@ -1,3 +1,9 @@
+include Irmin.Export_for_backends
+
+let src = Logs.Src.create "irmin.lavyek" ~doc:"Irmin Lavyek store"
+
+module Log = (val Logs.src_log src : Logs.LOG)
+
 module Conf = struct
   include Irmin.Backend.Conf
 
@@ -5,147 +11,137 @@ module Conf = struct
   let root config = find_root config |> Option.value ~default:"."
 end
 
-module Helper (K : Irmin.Type.S) (V : Irmin.Type.S) = struct
-  module Tbl = Hashtbl.Make (struct
+module Read_only (K : Irmin.Type.S) (V : Irmin.Type.S) = struct
+  module KMap = Map.Make (struct
     type t = K.t
 
-    let equal a b = Irmin.Type.(unstage (equal K.t)) a b
-    let hash k = Irmin.Type.(unstage (short_hash K.t)) k
+    let compare = Irmin.Type.(unstage (compare K.t))
   end)
 
-  type 'a t = V.t Tbl.t (* Store type: a hashtable mapping keys to values *)
-  type key = K.t (* Key type *)
-  type value = V.t (* Value type *)
+  type key = K.t
+  type value = V.t
+  type 'a t = { mutable t : value KMap.t }
 
-  let spec = Irmin.Backend.Conf.Spec.v "tutorial"
-  let init_size = Irmin.Backend.Conf.key ~spec "init-size" Irmin.Type.int 8
+  let new_instance _root = { t = KMap.empty }
 
-  let v config =
-    let module C = Irmin.Backend.Conf in
-    let init_size = C.get config init_size in
-    Tbl.create init_size
+  let v =
+    let cache : (string, 'a t) Hashtbl.t = Hashtbl.create 0 in
+    fun config ->
+      let root = Conf.root config in
+      let t =
+        match Hashtbl.find_opt cache root with
+        | None ->
+            let t = new_instance root in
+            Hashtbl.add cache root t;
+            t
+        | Some t -> t
+      in
+      t
 
-  let mem t key = Tbl.mem t key
-  let find t key = Tbl.find_opt t key
-  let clear t = Tbl.clear t
+  let clear t =
+    [%log.debug "clear"];
+    t.t <- KMap.empty
+
+  let close _ =
+    [%log.debug "close"];
+    ()
+
+  let cast t = (t :> read_write t)
+  let batch t f = f (cast t)
+  let pp_key = Irmin.Type.pp K.t
+
+  let find { t; _ } key =
+    [%log.debug "find %a" pp_key key];
+    try Some (KMap.find key t) with Not_found -> None
+
+  let mem { t; _ } key =
+    [%log.debug "mem %a" pp_key key];
+    KMap.mem key t
 end
 
-module Content_addressable : Irmin.Content_addressable.Maker =
-functor
-  (K : Irmin.Hash.S)
-  (V : Irmin.Type.S)
-  ->
-  struct
-    include Helper (K) (V)
+module Append_only (K : Irmin.Type.S) (V : Irmin.Type.S) = struct
+  include Read_only (K) (V)
 
-    let encode_value = Irmin.Type.(unstage (to_bin_string V.t))
-    let unsafe_add t k v = Tbl.replace t k v
+  let add t key value =
+    [%log.debug "add -> %a" pp_key key];
+    t.t <- KMap.add key value t.t
+end
 
-    let add t value =
-      let hash = K.hash (fun f -> f (encode_value value)) in
-      unsafe_add t hash value;
-      print_endline (Irmin.Type.to_string K.t hash);
-      hash
+module Atomic_write (K : Irmin.Type.S) (V : Irmin.Type.S) = struct
+  module RO = Read_only (K) (V)
+  module W = Irmin.Backend.Watch.Make (K) (V)
+  module L = Irmin.Backend.Lock.Make (K)
 
-    let lock = Mutex.create ()
+  type t = { t : unit RO.t; w : W.t; lock : L.t }
+  type key = RO.key
+  type value = RO.value
+  type watch = W.watch
 
-    let batch t f =
-      Mutex.lock lock;
-      let x =
-        try f t
-        with exn ->
-          Mutex.unlock lock;
-          raise exn
-      in
-      Mutex.unlock lock;
-      x
+  let watches = W.v ()
+  let lock = L.v ()
 
-    let close _t = ()
-  end
+  let v config =
+    let t = RO.v config in
+    { t; w = watches; lock }
 
-module Atomic_write : Irmin.Atomic_write.Maker =
-functor
-  (K : Irmin.Type.S)
-  (V : Irmin.Type.S)
-  ->
-  struct
-    module H = Helper (K) (V)
-    module W = Irmin.Backend.Watch.Make (K) (V)
+  let close t =
+    W.clear t.w;
+    RO.close t.t
 
-    type t = { t : [ `Write ] H.t; w : W.t } (* Store type *)
-    type key = H.key (* Key type *)
-    type value = H.value (* Value type *)
-    type watch = W.watch (* Watch type *)
+  let find t = RO.find t.t
+  let mem t = RO.mem t.t
+  let watch_key t = W.watch_key t.w
+  let watch t = W.watch t.w
+  let unwatch t = W.unwatch t.w
 
-    let watches = W.v ()
+  let list t =
+    [%log.debug "list"];
+    RO.KMap.fold (fun k _ acc -> k :: acc) t.t.RO.t []
 
-    let v config =
-      let t = H.v config in
-      { t; w = watches }
+  let set t key value =
+    [%log.debug "update"];
+    L.with_lock t.lock key (fun () ->
+        t.t.RO.t <- RO.KMap.add key value t.t.RO.t);
+    W.notify t.w key (Some value)
 
-    let find t = H.find t.t
-    let mem t = H.mem t.t
-    let watch_key t key = W.watch_key t.w key
-    let watch t = W.watch t.w
-    let unwatch t = W.unwatch t.w
-    let list { t; _ } = H.Tbl.to_seq_keys t |> List.of_seq
+  let remove t key =
+    [%log.debug "remove"];
+    L.with_lock t.lock key (fun () -> t.t.RO.t <- RO.KMap.remove key t.t.RO.t);
+    W.notify t.w key None
 
-    let set { t; w } key value =
-      print_endline "set0";
-      let exists = H.Tbl.mem t key in
-      print_endline "set1";
-      H.Tbl.replace t key value;
-      print_endline "set2";
-      if exists then W.notify w key (Some value);
-      print_endline "set3"
+  let equal_v_opt = Irmin.Type.(unstage (equal (option V.t)))
 
-    let remove { t; w } key =
-      H.Tbl.remove t key;
-      W.notify w key None
+  let test_and_set t key ~test ~set =
+    [%log.debug "test_and_set"];
+    let updated =
+      L.with_lock t.lock key (fun () ->
+          let v = find t key in
+          if equal_v_opt test v then
+            let () =
+              match set with
+              | None -> t.t.RO.t <- RO.KMap.remove key t.t.RO.t
+              | Some v -> t.t.RO.t <- RO.KMap.add key v t.t.RO.t
+            in
+            true
+          else false)
+    in
+    if updated then W.notify t.w key set;
+    updated
 
-    let value_equal = Irmin.Type.(unstage (equal (option V.t)))
-
-    let test_and_set { t; w } key ~test ~set:set_value =
-      print_endline "testset0";
-      let v = H.Tbl.find_opt t key in
-      print_endline "testset1";
-      if value_equal v test then (
-        let () =
-          print_endline "testset2";
-          match set_value with
-          | Some set_value -> H.Tbl.replace t key set_value
-          | None -> H.Tbl.remove t key
-        in
-        print_endline "testset3";
-        W.notify w key set_value;
-        print_endline "testset4";
-        true)
-      else (
-        print_endline "testset5";
-        false)
-
-    let clear { t; _ } = H.Tbl.clear t
-    let close _t = ()
-  end
-
-module Maker : Irmin.Maker = Irmin.Maker (Content_addressable) (Atomic_write)
-
-module KV = struct
-  type endpoint = unit
-  type metadata = unit
-
-  module Make (C : Irmin.Contents.S) = struct
-    include Maker.Make (struct
-      module Info = Irmin.Info.Default
-      module Metadata = Irmin.Metadata.None
-      module Contents = C
-      module Path = Irmin.Path.String_list
-      module Branch = Irmin.Branch.String
-      module Hash = Irmin.Hash.SHA1
-      module Node = Irmin.Node.Make (Hash) (Path) (Metadata)
-      module Commit = Irmin.Commit.Make (Hash)
-    end)
-  end
+  let clear t =
+    W.clear t.w;
+    RO.clear t.t
 end
 
 let config () = Conf.empty Conf.spec
+
+module Content_addressable = Irmin.Content_addressable.Make (Append_only)
+module S = Irmin.Maker (Content_addressable) (Atomic_write)
+module KV = Irmin.KV_maker (Content_addressable) (Atomic_write)
+include S
+
+(* Enforce that {!S} is a sub-type of {!Irmin.Maker}. *)
+module Maker_is_a_maker : Irmin.Maker = S
+
+(* Enforce that {!KV} is a sub-type of {!Irmin.KV_maker}. *)
+module KV_is_a_KV : Irmin.KV_maker = KV
