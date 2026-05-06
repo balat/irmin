6e18d356c0fe94517f8cc91bdf8ccf3367e55b74
Vincent Balat <vincent.balat@vblt.org>
2026-05-06 17:51:48 +0200

    irmin-lwt-chunk: wire the irmin-lwt-test harness (29/29 tests)
    Combines [Irmin_lwt_chunk.Content_addressable (Irmin_lwt_mem.Append_only)]
    with [Irmin_lwt_mem.Atomic_write] to obtain a full
    [Irmin_lwt.Maker], and runs the harness on it: 29/29 tests pass.

--- test/irmin-lwt-chunk/test.ml
+++ test/irmin-lwt-chunk/test.ml
@@ -22,9 +22,10 @@
     the default chunk size threshold. *)
 
 module CA = Irmin_lwt_chunk.Content_addressable (Irmin_lwt_mem.Append_only)
-module Maker = Irmin_lwt.Maker (CA) (Irmin_lwt_mem.Atomic_write)
+module Chunk_maker = Irmin_lwt.Maker (CA) (Irmin_lwt_mem.Atomic_write)
 module S = Irmin_lwt.KV_maker (CA) (Irmin_lwt_mem.Atomic_write)
 module Store = S.Make (Irmin_lwt.Contents.String)
+module Irmin_test = Irmin_lwt_test.Irmin_test
 open Lwt.Syntax
 
 let info ?(author = "test") msg =
@@ -54,6 +55,23 @@ let run name f =
 
 let () =
   Eio_main.run @@ fun env ->
+  Eio.Switch.run @@ fun sw ->
+  Irmin.Backend.Watch.set_watch_switch sw;
   Lwt_eio.with_event_loop ~clock:env#clock @@ fun _ ->
   run "chunked set/get small+big" test_basic;
-  print_endline "irmin-lwt-chunk: smoke test passed"
+  print_endline "--- running irmin-lwt-test harness ---";
+  let suite =
+    let store =
+      Irmin_test.store (module Chunk_maker) (module Irmin_lwt.Metadata.None)
+    in
+    let init ~config:_ = Lwt.return_unit in
+    let config =
+      Irmin_lwt_chunk.config ~size:512 ~min_size:131 (Irmin_lwt_mem.config ())
+    in
+    Irmin_test.Suite.create ~name:"CHUNK" ~init ~store ~config ()
+  in
+  Lwt_eio.Promise.await_lwt
+    (Irmin_test.Store.run "irmin-lwt-chunk" ~slow:false ~misc:[]
+       ~sleep:Lwt_unix.sleep
+       [ (`Quick, suite) ]);
+  print_endline "irmin-lwt-chunk: all tests passed"
